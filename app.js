72d092433f4fd494a9a494a1f000e83f87cd357e
Pablo Campos <pablo.campospaula00@gmail.com>
2024-09-09 02:36:53 -0300

    colocando as imagens

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function pesquisar() {
 
     // se campoPesquisa for uma string sem nada
     if (!campoPesquisa) {
-        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um Planeta</p>"
+        section.innerHTML = `<p class="nada">Nada foi encontrado. Você precisa digitar o nome de um Planeta</p>`
         return 
     }
 
@@ -24,8 +24,9 @@ function pesquisar() {
         titulo = dado.titulo.toLowerCase()
         descricao = dado.descricao.toLowerCase()
         tags = dado.tags.toLowerCase()
+        imagem = dado.tags.toLowerCase()
         // se titulo includes campoPesquisa
-        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa) || imagem.includes(campoPesquisa)) {
             // cria um novo elemento
             resultados += `
             <div class="item-resultado">
@@ -34,13 +35,16 @@ function pesquisar() {
                 </h2>
                 <p class="descricao-meta">${dado.descricao}</p>
                 <a href=${dado.link} target="_blank">Mais informações</a>
+                <div class="planeta-container">
+                <img src=${dado.imagem}>
+                </div>
             </div>
         `;
         }
     }
 
     if (!resultados) {
-        resultados = "<p>Nada foi encontrado</p>"
+        resultados = `<p class="nada">Nada foi encontrado</p>`
     }
 
     // Atribui os resultados gerados à seção HTML
--- dados.js
+++ dados.js
@@ -3,48 +3,56 @@ let dados = [
         titulo: "Mercúrio",
         descricao: "Mercúrio é um planeta superinteressante e cheio de curiosidades! Ele é o menor planeta do nosso Sistema Solar e também o mais próximo do Sol. Isso significa que ele recebe muita energia solar e suas temperaturas são bem extremas: durante o dia, pode chegar a ser super quente, mas à noite, fica muito frio!",
         link: "https://science.nasa.gov/mercury/",
-        tags: "um menor mercurio pequeno"
+        tags: "um menor mercurio pequeno, planeta, ordem",
+        imagem: "mercurio.png"
     },
     {
         titulo: "Vênus",
         descricao: "Vênus é conhecido como o gêmeo da Terra por ser parecido em tamanho e massa, mas é muito mais quente e tem uma atmosfera densa de nuvens que causam um efeito estufa super forte.",
         link: "https://science.nasa.gov/venus/",
-        tags: "quente dois segundo venus"
+        tags: "quente dois segundo venus, planeta, ordem",
+        imagem: "venus.png"  
     },
     {
         titulo: "Terra",
         descricao: "Nosso lar! O único planeta que sabemos ter vida. Aqui temos água, ar puro e uma grande variedade de plantas e animais.",
         link: "https://science.nasa.gov/earth/",
-        tags: "vida lar terra terceiro"
+        tags: "vida lar terra terceiro, planeta, ordem",
+        imagem: "terra.png"  
     },
     {
         titulo: "Marte",
         descricao: " Marte é conhecido como o Planeta Vermelho por causa da cor de sua superfície. Os cientistas acreditam que Marte já teve água e talvez até vida no passado.",
         link: "https://science.nasa.gov/mars/",
-        tags: "vermelho quarto"
+        tags: "vermelho quarto, planeta, ordem",
+        imagem: "marte.png"  
     },
     {
         titulo: "Jupíter",
         descricao: "É o maior planeta do Sistema Solar! Ele é feito principalmente de gases e tem uma grande mancha vermelha, que é uma tempestade gigante que dura há séculos.",
         link: "https://science.nasa.gov/jupiter/",
-        tags: "maior furacao quinto"
+        tags: "maior furacao quinto, jupiter, planeta, ordem",
+        imagem: "jupiter.png"  
     },
     {
         titulo: "Saturno",
         descricao: " Saturno é famoso pelos seus anéis, que são feitos de gelo e poeira. É o segundo maior planeta do Sistema Solar.",
         link: "https://science.nasa.gov/saturn/",
-        tags: "aneis anéis sexto"
+        tags: "aneis anéis sexto, planeta, ordem",
+        imagem: "saturno.png"  
     },
     {
         titulo: "Urano",
         descricao: " Urano é um planeta bem diferente dos outros. Ele gira de lado e tem um sistema de anéis bem fino.",
         link: "https://science.nasa.gov/uranus/",
-        tags: "azul gelo penultimo aneis sétimo"
+        tags: "azul gelo penultimo aneis sétimo, planeta, ordem",
+        imagem: "urano.png"  
     },
     {
         titulo: "Netuno",
         descricao: "Netuno é o planeta mais distante do Sol e é muito frio. Ele é azul por causa dos gases em sua atmosfera.",
         link: "https://science.nasa.gov/neptune/",
-        tags: "ultimo azul oitavo"
+        tags: "ultimo azul oitavo, planeta, ordem",
+        imagem: "netuno.png"  
     }
 ];
